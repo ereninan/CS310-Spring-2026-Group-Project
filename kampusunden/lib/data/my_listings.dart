05f27d3d4e4fd2c741132f4d354ca06ee85a26b4
apoliko <64897230+apoliko@users.noreply.github.com>
2026-04-13 19:20:05 +0300

    data file has been added to project

---
+++ kampusunden/lib/data/my_listings.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> dummyAds = [
+{
+"title": "Macbook M3",
+"price": "621",
+"imageUrl": "https://static.ticimax.cloud/cdn-cgi/image/width=574,quality=85/54992/uploads/urunresimleri/buyuk/macbook-pro-13-8-core-cpu-8-core-gpu-a-b45b-4.jpeg"
+},
+{
+"title": "Samsung S24",
+"price": "550",
+"imageUrl": "https://cdn.akakce.com/z/samsung/samsung-galaxy-s24-ultra-256-gb.jpg"
+},
+  {
+    "title": "Samsung S24",
+    "price": "550",
+    "imageUrl": "https://cdn.akakce.com/z/samsung/samsung-galaxy-s24-ultra-256-gb.jpg"
+  },
+
+];
